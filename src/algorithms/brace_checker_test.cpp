72505b217be69d2b735398c304ba3d74ee1e58dc
Sopniewski Michal <michal.sopniewski@nokia.com>
2018-02-14 12:36:23 +0100

    brace checker algorithm added into example

---
+++ src/algorithms/brace_checker_test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include "brace_checker.h"
+
+using namespace algorithms;
+
+class brace_checker_test : public ::testing::Test
+{
+public:
+    brace_checker_test
+() {}
+};
+
+TEST_F(brace_checker_test, check_braces_on_empty_string){
+    EXPECT_TRUE(check_brace_correctness(""));
+    EXPECT_TRUE(check_brace_correctness("xdsadsa")); 
+    EXPECT_FALSE(check_brace_correctness("{"));
+    EXPECT_TRUE(check_brace_correctness("{}"));
+    EXPECT_FALSE(check_brace_correctness("}"));
+    EXPECT_FALSE(check_brace_correctness("{[}]"));
+    EXPECT_TRUE(check_brace_correctness("{aa[dss(xxx)(sss)]dd[ss{dd}dd]aa}"));
+}
+
