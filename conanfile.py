7dca8c4ee7f59b743f12629751875fc2e883384f
Michal Sopniewski <michal.sopniewski@gmail.com>
2020-11-14 21:16:29 +0100

    set gtest to 1.8.1 fix generators

--- conanfile.py
+++ conanfile.py
@@ -2,8 +2,8 @@ from conans import ConanFile, CMake
 
 class PocoTimerConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "gtest/1.10.0"
-   generators = "cmake","cmake_paths"
+   requires = "gtest/1.8.1@bincrafters/stable" # package 1.10.0 have some issue witho conanfile ... find package dosn't work with build_type=Debug
+   generators = "cmake", "cmake_paths"
 
    def imports(self):
       self.copy("*.dll", dst="bin", src="bin") # From bin to bin
