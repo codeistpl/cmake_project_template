ebca551767bf35eddcfb5a41119f3ed75a4d8240
Sopniewski Michal <michal.sopniewski@aptiv.com>
2026-02-09 18:51:16 +0100

    added clang build

---
+++ build_with_clang.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+if [ ! -d ".build_clang" ]; then
+    mkdir -p .build_clang
+fi
+
+cd .build_clang
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+cmake --build .
