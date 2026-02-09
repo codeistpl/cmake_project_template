ead2b1d2f641a78ca5f7238403bf349104116a65
Sopniewski Michal <michal.sopniewski@aptiv.com>
2026-02-09 19:24:50 +0100

    paralel build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ fi
 
 cd .build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
-cmake --build .
+cmake --build . --parallel
--- build_with_clang.sh
+++ build_with_clang.sh
@@ -5,4 +5,4 @@ fi
 
 cd .build_clang
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
-cmake --build .
+cmake --build .  --parallel
